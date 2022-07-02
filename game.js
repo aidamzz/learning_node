992de2bd625bb97e61782f35d7e6ea68f22c14b2
aidamzz <aidamohagheghzade@gmail.com>
2022-07-02 23:36:19 +0430

    prototypes

--- game.js
+++ game.js
@@ -6,4 +6,27 @@ function User(){
         console.log(this.name + "gave 1 life to "+ targetPlayer.name);
     }
 }
-var
+var Bucky = new User();
+var Wendy = new User();
+Bucky.name = "Bucky";
+Wendy.name = "Wendy";
+
+Bucky.giveLife(Wendy);
+console.log("Bucky: "+ Bucky.life)
+console.log("Wendy: "+ Wendy.life)
+
+//you can add functions to all objects
+User.prototype.uppercut = function uppercut(targetPlayer){
+    targetPlayer.life -= 3;
+    console.log(this.name+ " just uppercutted "+ targetPlayer.name)
+
+};
+
+Wendy.uppercut(Bucky);
+console.log("Bucky: "+ Bucky.life);
+console.log("Wendy: "+ Wendy.life);
+
+//You can add properties to all objects
+User.prototype.magic = 60;
+console.log(Bucky.magic);
+console.log(Wendy.magic);
